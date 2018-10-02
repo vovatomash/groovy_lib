a0979fd21948253a19add64aad8019ab3209e4b5
Volodymyr Tomash <vova.s.tomash@gmail.com>
2018-10-02 16:06:18 +0300

    test1

---
+++ src/Lib.groovy
@@ -0,0 +1,15 @@
+def stageDecorator(String name, Closure c){
+    def r
+    try {
+        r = c()
+    } catch (hudson.AbortException er) {
+        println('result')
+        println(er)
+        println(r)
+         throw er
+    } catch (Exception er1) {
+        println(er1)
+        throw er1
+    }
+    return r
+}
