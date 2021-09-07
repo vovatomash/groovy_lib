bf87dcba3e3de1419e3b7812e3f689d3d383b41b
Volodymyr Tomash <vova.s.tomash@gmail.com>
2021-09-07 18:19:01 +0300

    fix

--- src/devops/common/NexusLib.groovy
+++ src/devops/common/NexusLib.groovy
@@ -5,12 +5,12 @@ import groovy.json.JsonOutput
 
 public class NexusLib implements Serializable {
     String nexusUrl
-    String nexusApiEndpoint
+    //String nexusApiEndpoint
 
 
     def NexusLib(String nexusUrl) {
         this.nexusUrl = nexusUrl
-        this.nexusApiEndpoint = nexusUrl + '/service/rest/v1/search?sort=version'
+        //this.nexusApiEndpoint = nexusUrl + '/service/rest/v1/search?sort=version'
     }
 
 
