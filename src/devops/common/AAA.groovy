e3a99d1e7efd3a98d6e928841cbf5c57809e248d
Volodymyr Tomash <vova.s.tomash@gmail.com>
2021-09-07 18:22:09 +0300

    fix

---
+++ src/devops/common/AAA.groovy
@@ -0,0 +1,16 @@
+package devops.common;
+
+import groovy.json.JsonOutput
+
+
+public class AAA implements Serializable {
+    String nexusUrl
+
+
+    def AAA(String nexusUrl) {
+        this.nexusUrl = nexusUrl
+    }
+
+
+} //end class
+
